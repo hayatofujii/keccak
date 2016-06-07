f2cbf0892a4102914c7129a53bca9da29d913668
Hayato Fujii <hayato.fujii@gmail.com>
2016-06-07 12:29:05 -0300

    fix compilation

--- keccak.c
+++ keccak.c
@@ -3,7 +3,6 @@
 
 // https://github.com/gvanas/KeccakCodePackage/blob/master/Standalone/CompactFIPS202/Keccak-readable-and-compact.c
 #define ROT64(a, offset) ((((uint64_t)a) << offset) ^ (((uint64_t)a) >> (64 - offset)))
-#define i(x, y) ((x) + 5 * (y))
 
 // EXPERIMENTAL: isso ou fazer a LSFR?
 // rc[t] = (x^t mod x^8 + x^6 + x^5 + x^4 + 1) mod x in GF(2)[x].
@@ -21,6 +20,17 @@ uint64_t keccak_rc[24] = {
     0x8000000080008081, 0x8000000000008080,
     0x0000000080000001, 0x8000000080008008,
 };
+
+// (t + 1)(t + 2) / 2
+// 0 < t < 24
+uint32_t keccak_rotation[5][5] = {
+    {25, 39,  3, 10, 43},
+    {55, 20, 36, 44, 6 },
+    {28, 27,  0,  1, 62},
+    {56, 14, 18,  2, 61},
+    {21, 8 , 41, 45, 15}
+};
+
 
 void keccak_f(state A)
 {
@@ -44,18 +54,19 @@ void round_b(state A, int r)
 void theta(state A)
 {
     uint64_t C[5];
-    uint64_t D[5];
+    uint64_t D;
     int x, y;
 
     for (x = 0; x < 5; x++)
         C[x] = A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4];
 
     for (x = 0; x < 5; x++)
-        D[x] = C[x - 1] ^ ROT64(C[x + 1], 1);
+    {
+        D = C[(x + 4) % 5] ^ ROT64(C[(x + 1) % 5], 1);
 
-    for (x = 0; x < 5; x++)
         for (y = 0; y < 5; y++)
-            A[x][y] ^= D[x];
+            A[x][y] ^= D;
+    }
 }
 
 void rho_pi(state A, state B)
@@ -64,7 +75,7 @@ void rho_pi(state A, state B)
 
     for (x = 0; x < 5; x++)
         for (y = 0; y < 5; y++)
-            B[y][2 * x + 3 * y] = ROT64(A[x][y], keccak_rc[i(x, y)]);
+            B[y][(2 * x + 3 * y) % 5] = ROT64(A[x][y], keccak_rotation[x][y]);
 }
 
 void chi(state A, state B)
@@ -73,7 +84,7 @@ void chi(state A, state B)
 
     for (x = 0; x < 5; x++)
         for (y = 0; y < 5; y++)
-           A[x][y] = B[x][y] ^ (~B[x + 1][y] & B[x + 2][y]);
+           A[x][y] = B[x][y] ^ (~B[(x + 1) % 5][y] & B[(x + 2) % 5][y]);
 
 }
 
